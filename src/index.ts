878a406ece9eb4f257f768a392df7d6e11f91ee2
Alex Souza <alex.webdevelloper@gmail.com>
2025-03-20 20:54:38 -0300

    :sparkles: feat: Add factorial and binomial probability functions

--- src/index.ts
+++ src/index.ts
@@ -124,3 +124,47 @@ export function standardize(x: number[]): number[] {
 
   return x.map((val) => zScore(val, _mean, _std));
 }
+
+/**
+ * Calculates the factorial of a number
+ * @param n The number to calculate the factorial for
+ * @returns The factorial of the input number
+ * @throws An error if the input number is negative.
+ */
+export function factorial(n: number): number {
+  if (n < 0) {
+    throw new Error();
+  }
+
+  if (n <= 1) {
+    return 1;
+  }
+
+  let total = n;
+
+  for (let i = n - 1; i > 0; i--) {
+    total *= i;
+  }
+
+  return total;
+}
+
+/**
+ * Calculate the binomial probability of a number of successes in a fixed number of trials
+ * @param n The number of trials
+ * @param k The number of successful trials
+ * @param p The probability of success on each trial
+ * @returns The binomial probability of the input parameters
+ * @throws An error if the number of successful trials is greater than the number of trials.
+ */
+export function binomial(n: number, k: number, p: number): number {
+  if (k > n) {
+    throw new Error();
+  }
+
+  const n_diff_k = n - k;
+  const coefficient = factorial(n) / (factorial(k) * factorial(n_diff_k));
+  const q = 1 - p;
+
+  return coefficient * Math.pow(p, k) * Math.pow(q, n_diff_k);
+}
