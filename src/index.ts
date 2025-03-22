30e13a62c4c7465fa133f927ea64b3ac69bde387
Alex Souza <alex.webdevelloper@gmail.com>
2025-03-21 21:51:51 -0300

    :sparkles: feat: Enhance one-hot encoding with dropLast option and improve handling of undefined values

--- src/index.ts
+++ src/index.ts
@@ -190,12 +190,14 @@ export function poisson(lambda: number, k: number): number {
  * Encode categorical columns in a dataset using the `one-hot encoding` technique.
  * @param rows An array of objects representing the dataset
  * @param columns An array of column names to encode
+ * @param dropLast A boolean indicating whether to drop the last column in the encoding **(useful for linear models to avoid multicollinearity)**
  * @returns An object containing the encoded dataset and the new column names
  * @throws An error if the input dataset is empty or if the input columns are not present in the dataset.
  */
 export function oneHotEncoding(
   rows: any[],
-  columns: string[]
+  columns: string[],
+  dropLast: boolean = false
 ): { rows: any[]; columns: string[] } {
   const columnValues = new Map<string, Set<any>>();
 
@@ -216,7 +218,11 @@ export function oneHotEncoding(
   const columnMapping = new Map<string, { column: string; value: any }>();
 
   for (const column of columns) {
-    for (const value of columnValues.get(column)!) {
+    const values = Array.from(columnValues.get(column)!);
+    if (dropLast) {
+      values.pop();
+    }
+    for (const value of values) {
       const newColumnName = `${column}_${value}`;
       newColumns.push(newColumnName);
       columnMapping.set(newColumnName, { column, value });
@@ -227,7 +233,11 @@ export function oneHotEncoding(
   const encodedRows = rows.map((row) => {
     for (const newColumn of newColumns) {
       const { column: originalColumn, value } = columnMapping.get(newColumn)!;
-      row[newColumn] = row[originalColumn] === value ? 1 : 0;
+      if (row[originalColumn] !== undefined) {
+        row[newColumn] = row[originalColumn] === value ? 1 : 0;
+      } else {
+        row[newColumn] = 0;
+      }
     }
     return row;
   });
